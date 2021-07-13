aa12eabc03901319e333e265101a12b0668e8398
BelitskyYevgeniy <78808088+BelitskyYevgeniy@users.noreply.github.com>
2021-07-13 23:09:16 +0300

    divide Task 4/Program.cs

---
+++ Task 4/Syncronizer.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading;
+
+namespace Synchronization
+{
+    public class Syncronizer
+    {
+
+        protected AutoResetEvent _writeLocker = new AutoResetEvent(true);
+        protected ManualResetEvent _WriteEvent = new ManualResetEvent(true);
+        protected ManualResetEvent _ReadEvent = new ManualResetEvent(true);
+
+        public int WritersCount { get; private set; } = 0;
+        public int ReadersCount { get; private set; } = 0;
+
+        public void EnterWriter()
+        {
+            ++WritersCount;
+            _ReadEvent.Reset();
+            _WriteEvent.WaitOne();
+            _writeLocker.WaitOne();
+        }
+        public void ReleaseWriter()
+        {
+            --WritersCount;
+            if (WritersCount == 0)
+            {
+                _ReadEvent.Set();
+            }
+            _writeLocker.Set();
+        }
+
+        public void EnterReader()
+        {
+
+            _ReadEvent.WaitOne();
+            ++ReadersCount;
+            _WriteEvent.Reset();
+        }
+        public void ReleaseReader()
+        {
+            --ReadersCount;
+            if (ReadersCount == 0)
+            {
+                _WriteEvent.Set();
+            }
+        }
+            
+
+
+    }
+    
+}
--- Task 4/TestProgram.cs
+++ Task 4/TestProgram.cs
@@ -4,56 +4,11 @@ using System.Collections.Generic;
 namespace Synchronization
 {
 
-    class Program
+    class TestProgram
     {
-        public class Syncronizer
-        {
-            
-            protected AutoResetEvent _writeLocker = new AutoResetEvent(true);
-            protected ManualResetEvent _WriteEvent = new ManualResetEvent(true);
-            protected ManualResetEvent _ReadEvent = new ManualResetEvent(true);
-            public void EnterWriter()
-            {
-                ++WritersCount;
-                _ReadEvent.Reset();
-                _WriteEvent.WaitOne();
-                _writeLocker.WaitOne();
-            }
-            public void ReleaseWriter()
-            {
-                --WritersCount;
-                if(WritersCount == 0)
-                {
-                    _ReadEvent.Set();
-                }
-                _writeLocker.Set();
-            }
-
-            public void EnterReader()
-            {
-               
-                _ReadEvent.WaitOne();
-                ++ReadersCount;
-                _WriteEvent.Reset();
-            }
-            public void ReleaseReader()
-            {
-                --ReadersCount;
-                if(ReadersCount == 0)
-                {
-                    _WriteEvent.Set();
-                }
-            }
-            public int WritersCount { get; private set; } = 0;
-            public int ReadersCount { get; private set; } = 0;
-            
-
-        }
 
         static Syncronizer ts = new Syncronizer();
         static bool isAllowedToEnd = false;
-
-
 
         static void ThreadLog(string typeThread)
         {
