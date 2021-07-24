f265d456f9a678cf99eb987e5c25a9111ce5e4e1
BelitskyYevgeniy <78808088+BelitskyYevgeniy@users.noreply.github.com>
2021-07-24 16:08:48 +0300

    Add Task 7(Extensions)

--- dotNet/Task 5/Program.cs
+++ dotNet/Task 5/Program.cs
@@ -3,157 +3,169 @@ using System.Collections.Generic;
 using System.Linq;
 namespace DZ
 {
-    class Program
+    public enum SubjectType
     {
-        public enum SubjectType
+        Math,
+        English
+    }
+    [Serializable]
+    public struct Grade
+    {
+        private float _grade;
+        public SubjectType Subject { get; set; } //made for example of modification possibility 
+        public float Value
         {
-            Math,
-            English
-        }
-        [Serializable]
-        public struct Grade
-        {
-            private float _grade;
-            public SubjectType Subject { get; set; } //made for example of modification possibility 
-            public float Value
+            get => _grade;
+            set
             {
-                get => _grade;
-                set
+                if (value < 0 || value > 100)
                 {
-                    if (value < 0 || value > 100)
-                    {
-                        throw new ArgumentException();
-                    }
-                    _grade = value;
+                    throw new ArgumentException();
                 }
+                _grade = value;
             }
         }
+    }
 
-        [Serializable]
-        public class Student
-        {
-            public int Id { get; set; }
-            //something
-        }
-        [Serializable]
-        public class ElectronicBook
-        {
-            public delegate void StatisticFunc<T>(Grade grade, ref T data);
+    [Serializable]
+    public class Student
+    {
+        public int Id { get; set; }
+        //something
+    }
+    [Serializable]
+    public class ElectronicBook
+    {
+        public delegate void StatisticFunc<T>(Grade grade, ref T data);
 
-            public int StudentId { get; set; }
-            public IEnumerable<Grade> Grades { get; set; }
-            public ElectronicBook()
-            {
-                //Deserialization
-                Grades = new List<Grade>()
+        public int StudentId { get; set; }
+        public IEnumerable<Grade> Grades { get; set; }
+        public ElectronicBook()
+        {
+            //Deserialization
+            Grades = new List<Grade>()
                 {
                     new Grade{ Value = 50.5f, Subject = SubjectType.Math},
                     new Grade{ Value = 30.5f, Subject = SubjectType.English},
                     new Grade{ Value = 88.5f, Subject = SubjectType.Math}
 
                 };
-            }
-
-            public void ProcessBook<T>(StatisticFunc<T> func, ref T data)
-            {
-                foreach (var grade in Grades)
-                {
-                    func(grade, ref data);
-                }
-            }
         }
 
-        public static class AdditionFunctionality
+        public void ProcessBook<T>(StatisticFunc<T> func, ref T data)
         {
-            private class AverageStat
+            foreach (var grade in Grades)
             {
-                public float Result { get; set; }
-                public int Count { get; set; }
-            }
-            private static void IterAverage(Grade grab, ref AverageStat data)
-            {
-                data.Result += grab.Value;
-                ++data.Count;
-            }
-            private static void CheckLowest(Grade grab, ref float data)
-            {
-                if (grab.Value < data)
-                {
-                    data = grab.Value;
-                }
-            }
-            private static void CheckHighest(Grade grab, ref float data)
-            {
-                if (grab.Value > data)
-                {
-                    data = grab.Value;
-                }
-            }
-            public static double Average(ElectronicBook book)
-            {
-                if (book == null)
-                {
-                    throw new NullReferenceException();
-                };
-                var stat = new AverageStat { Result = 0f, Count = 0 };
-                book.ProcessBook<AverageStat>(IterAverage, ref stat);
-                return stat.Result / stat.Count;
-            }
-
-            public static float TakeHighest(ElectronicBook book)
-            {
-                if (book == null)
-                {
-                    throw new NullReferenceException();
-                };
-                float highest = 0f;
-                book.ProcessBook<float>(CheckHighest, ref highest);
-                return highest;
-            }
-            public static float TakeLowest(ElectronicBook book)
-            {
-                if (book == null)
-                {
-                    throw new NullReferenceException();
-                };
-                float lowest = 100f;
-                book.ProcessBook<float>(CheckLowest, ref lowest);
-                return lowest;
+                func(grade, ref data);
             }
         }
-        public static class AdditionFunctionalityUsingLINQ
+    }
+
+    public static class ElectronicBookExtensions
+    {
+        public static double Average(this ElectronicBook book)
         {
-            
-            
-            public static double Average(ElectronicBook book)
+            if (book == null)
             {
-                if (book == null)
-                {
-                    throw new NullReferenceException();
-                };
-                var sum = book.Grades.Sum(grade => grade.Value);
-                return sum / book.Grades.Count();
-            }
-
-            public static float TakeHighest(ElectronicBook book)
-            {
-                if (book == null)
-                {
-                    throw new NullReferenceException();
-                };
-                return book.Grades.Max(grade => grade.Value);
-            }
-            public static float TakeLowest(ElectronicBook book)
-            {
-                if (book == null)
-                {
-                    throw new NullReferenceException();
-                };
-                return book.Grades.Min(grade => grade.Value);
-            }
+                throw new NullReferenceException();
+            };
+            var sum = book.Grades.Sum(grade => grade.Value);
+            return sum / book.Grades.Count();
         }
 
+        public static float TakeHighest(this ElectronicBook book)
+        {
+            if (book == null)
+            {
+                throw new NullReferenceException();
+            };
+            return book.Grades.Max(grade => grade.Value);
+        }
+        public static float TakeLowest(this ElectronicBook book)
+        {
+            if (book == null)
+            {
+                throw new NullReferenceException();
+            };
+            return book.Grades.Min(grade => grade.Value);
+        }
+    }
+    public static class AdditionFunctionality
+    {
+        private class AverageStat
+        {
+            public float Result { get; set; }
+            public int Count { get; set; }
+        }
+        private static void IterAverage(Grade grab, ref AverageStat data)
+        {
+            data.Result += grab.Value;
+            ++data.Count;
+        }
+        private static void CheckLowest(Grade grab, ref float data)
+        {
+            if (grab.Value < data)
+            {
+                data = grab.Value;
+            }
+        }
+        private static void CheckHighest(Grade grab, ref float data)
+        {
+            if (grab.Value > data)
+            {
+                data = grab.Value;
+            }
+        }
+        public static double Average(ElectronicBook book)
+        {
+            if (book == null)
+            {
+                throw new NullReferenceException();
+            };
+            var stat = new AverageStat { Result = 0f, Count = 0 };
+            book.ProcessBook<AverageStat>(IterAverage, ref stat);
+            return stat.Result / stat.Count;
+        }
 
+        public static float TakeHighest(ElectronicBook book)
+        {
+            if (book == null)
+            {
+                throw new NullReferenceException();
+            };
+            float highest = 0f;
+            book.ProcessBook<float>(CheckHighest, ref highest);
+            return highest;
+        }
+        public static float TakeLowest(ElectronicBook book)
+        {
+            if (book == null)
+            {
+                throw new NullReferenceException();
+            };
+            float lowest = 100f;
+            book.ProcessBook<float>(CheckLowest, ref lowest);
+            return lowest;
+        }
+    }
+    public static class AdditionFunctionalityUsingLINQ
+    {
+        public static double Average(ElectronicBook book)
+        {
+            return AdditionFunctionalityUsingLINQ.Average(book);
+        }
+
+        public static float TakeHighest(ElectronicBook book)
+        {
+            return AdditionFunctionalityUsingLINQ.TakeHighest(book);
+        }
+        public static float TakeLowest(ElectronicBook book)
+        {
+            return AdditionFunctionalityUsingLINQ.TakeLowest(book);
+        }
+    }
+    public class Program
+    {
         static void Main(string[] args)
         {
             ElectronicBook book = new ElectronicBook();
@@ -164,7 +176,11 @@ namespace DZ
             Console.WriteLine(AdditionFunctionalityUsingLINQ.Average(book));
             Console.WriteLine(AdditionFunctionalityUsingLINQ.TakeHighest(book));
             Console.WriteLine(AdditionFunctionalityUsingLINQ.TakeLowest(book));
+
+
+            Console.WriteLine(book.Average());
+            Console.WriteLine(book.TakeHighest());
+            Console.WriteLine(book.TakeLowest());
         }
     }
 }
-
